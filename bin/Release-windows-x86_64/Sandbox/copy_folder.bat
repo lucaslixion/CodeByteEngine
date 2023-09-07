6758f704619565e59fdf9533e4adfe833429f0b8
lucas <lucashooghof@gmail.com>
2023-09-07 14:57:53 +0200

    The Great Reset

--- bin/Release-windows-x86_64/Sandbox/copy_folder.bat
+++
@@ -1,9 +0,0 @@
-REM Replace with your actual folder paths
-SET source_folder=C:\dev\CodeByteEngine\CodeByteEngine\
-SET destination_folder=C:\dev\CodeByteEngine\bin\Release-windows-x86_64\Sandbox
-
-REM Copy the entire folder and its contents
-for %%i in ("%source_folder%\*.hlsl") do (
-    xcopy /Y "%%i" "%destination_folder%\"
-)
-PAUSE
